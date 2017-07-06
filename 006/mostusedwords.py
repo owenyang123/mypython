b9cbe6658b63a1b9322535212e9458c9528d1ffa
owenyang123 <owenyang15@gmail.com>
2017-07-06 23:40:08 +0900

    006

---
+++ 006/mostusedwords.py
@@ -0,0 +1,30 @@
+import os
+import re
+
+def findWord(DirPath):
+    if not os.path.isdir(DirPath):
+        return
+    fileList = os.listdir(DirPath)
+    reObj = re.compile('\b?(\w+)\b?')
+    for file in fileList:
+        filePath = os.path.join(DirPath, file)
+        if os.path.isfile(filePath) and os.path.splitext(filePath)[1] == '.txt':
+            with open(filePath) as f:
+                data = f.read()
+                words = reObj.findall(data)
+                wordDict = dict()
+                for word in words:
+                    word = word.lower()
+                    if word in ['a', 'the', 'to']:
+                        continue
+                    if word in wordDict:
+                        wordDict[word] += 1
+                    else:
+                        wordDict[word] = 1
+            ansList = sorted(wordDict.items(), key=lambda t: t[1], reverse=True)
+            print('file: %s->the most word: %s' % (file, ansList[1]))
+
+
+if __name__ == '__main__':
+    path=raw_input('please input path: ')
+    findWord(path)
---
+++ findfile.py
@@ -0,0 +1,35 @@
+import os
+import csv
+import re
+path123=raw_input('please input path: ')
+reg=r'(\.doc$)'
+pdfre=re.compile(reg)
+for path,d,filelist in os.walk(path123):
+    for filename in filelist:
+        t=os.path.join(path,filename)
+        if re.findall(pdfre,t):
+            print t
+
+reg=r'(\.docx$)'
+pdfre=re.compile(reg)
+for path,d,filelist in os.walk(path123):
+    for filename in filelist:
+        t=os.path.join(path,filename)
+        if re.findall(pdfre,t):
+            print t
+
+reg=r'(\.txt$)'
+pdfre=re.compile(reg)
+for path,d,filelist in os.walk(path123):
+    for filename in filelist:
+        t=os.path.join(path,filename)
+        if re.findall(pdfre,t):
+            print t
+
+reg=r'(\.pdf$)'
+pdfre=re.compile(reg)
+for path,d,filelist in os.walk(path123):
+    for filename in filelist:
+        t=os.path.join(path,filename)
+        if re.findall(pdfre,t):
+            print t
