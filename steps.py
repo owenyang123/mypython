d3ea83c34f9238d0ab3fc98828e60bd07a08c6aa
owenyang123 <owenyang15@gmail.com>
2018-03-08 21:23:41 +0900

    fixed step

--- steps.py
+++ steps.py
@@ -4,8 +4,9 @@ import random
 step_num=int(input("input max steps:"))
 
 l=[]
-for x in range(1, step_num+2):
+for x in range(0, step_num+1):
     l.append(x)
+print l
 l[0]=1
 l[1]=1
 l[2]=2
@@ -13,6 +14,5 @@ l[3]=4
 for x in range(4,step_num+1):
     t=l[x-1]+l[x-2]+l[x-3]
     l[x]=t
-    print x
-    print l,l.__len__()
+    print x,l
 
