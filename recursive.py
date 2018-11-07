88b8c47368d6eabbb1ae9f6d1f450da942946f00
owenyang123 <owenyang15@gmail.com>
2018-11-07 00:07:06 -0500

    dfs
    fix some bugs

--- recursive.py
+++ recursive.py
@@ -1,66 +1,6 @@
 import numpy as np
 import random
 
-
-from collections import defaultdict
-
-
-# This class represents a directed graph
-# using adjacency list representation
-class Graph:
-
-    def __init__(self, vertices):
-        # No. of vertices
-        self.V = vertices
-
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
-
-    # function to add an edge to graph
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
-
-    '''A recursive function to print all paths from 'u' to 'd'. 
-    visited[] keeps track of vertices in current path. 
-    path[] stores actual vertices and path_index is current 
-    index in path[]'''
-
-    def printAllPathsUtil(self, u, d, visited, path):
-
-        # Mark the current node as visited and store in path
-        visited[u] = True
-        path.append(u)
-
-        # If current vertex is same as destination, then print
-        # current path[]
-        if u == d:
-            print path
-        else:
-            # If current vertex is not destination
-            # Recur for all the vertices adjacent to this vertex
-            for i in self.graph[u]:
-                if visited[i] == False:
-                    self.printAllPathsUtil(i, d, visited, path)
-
-                # Remove current vertex from path[] and mark it as unvisited
-        path.pop()
-        visited[u] = False
-
-    # Prints all paths from 's' to 'd'
-    def printAllPaths(self, s, d):
-
-        # Mark all the vertices as not visited
-        visited = [False] * (self.V)
-
-        # Create an array to store paths
-        path = []
-
-        # Call the recursive helper function to print all paths
-        self.printAllPathsUtil(s, d, visited, path)
-
-    # Create a graph given in the above diagram
-
-
 def neighborlist(n):
     l=[]
     for x in range(n.shape[0]*n.shape[1]-1):
@@ -80,6 +20,7 @@ def graphmade(list1):
         else:
             graph.update({x[0]:[x[1],x[2]]})
     return graph
+
 #main function#
 #generate matrix#
 Matrix= np.zeros((5, 5))
@@ -96,14 +37,33 @@ tem2=Matrix.shape[1]-1
 n.append([tem1,tem2])
 nlist=neighborlist(Matrix)
 graph=graphmade(nlist)
+graph.update({24:[0]})
+def dfs_paths(graph,root,target,path=None):
+    if path is None:
+        path=[root]
+    if root==target:
+        yield path
+    for vertex in [x for x in graph[root] if x not in path]:
+        for each_path in dfs_paths(graph,vertex,target,path+[vertex]):
+            yield each_path
+y= list(dfs_paths(graph,13,24))
+i=0
+j=0
+sumall=[]
+for x in y:
+    sum=0
+    for k in x:
+        i=int(k/5)
+        j=k%5
+        sum=sum+Matrix[i][j]
+    x.append(sum)
+    sumall.append(sum)
+np.array(sumall)
+x=np,max(sumall)
+for i in y:
+    if i[-1]==x[1]:
+        print i
+print y
 
-g = Graph(Matrix.shape[0]*Matrix.shape[1])
+print graph
 
-for i in graph.keys():
-    for y in graph[i]:
-        g.addEdge(i,y)
-
-s = 0
-d = 24
-paths=g.printAllPaths(s, d)
-print paths
