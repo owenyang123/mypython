c2d5391d465cbee4ba23e79f5ed3f28c51e5d14a
owenyang123 <owenyang15@gmail.com>
2019-03-20 18:01:27 -0400

    1
    1

--- leetcode/8queue.py
+++ leetcode/8queue.py
@@ -1,4 +1,4 @@
-l= [[1] * 4 for i in range(4)]
+l= [[1] * 8 for i in range(8)]
 len1=len(l)
 
 
@@ -20,15 +20,15 @@ def set1(A,r,c):
         r1=r1-1
         c1=c1-1
         A[r1][c1] = 0
-    while(r2<len1 and c2<len1):
+    while(r2<len1-1 and c2<len1-1):
         A[r2][c2] = 0
         r2=r2+1
         c2=c2+1
-    while(r3<len1 and c3>=1):
+    while(r3<len1-1 and c3>=1):
         c3=c3-1
         r3=r3+1
         A[r3][c3] = 0
-    while(r4>=1 and c4<len1):
+    while(r4>=1 and c4<len1-1):
         r4=r4-1
         c4=c4+1
         A[r4][c4] = 0
@@ -36,24 +36,23 @@ def set1(A,r,c):
     return A
 
 count1=0
-B=l
+
 
 def dp(A,r,k):
     for i in range(len1):
+        if A[r][i]==1:
             set1(A, r, i)
-            if r==2 and (1 in A[r+1]):
-                r=0
-                k=k+1
-                print "x", A,r,k,i
-                A = [[1] * 4 for j in range(4)]
-            elif r==2:
-                r=0
-                print "y",A,r,i
-                A=[[1] * 4 for j in range(4)]
-
-            else:
-                r=r+1
-                print "z",A,r,i
-                dp(A,r,k)
+        if r==6 and (1 in A[r+1]):
+            r=0
+            k=k+1
+            print k,A,r,i
+            A = [[1] * 8 for j in range(8)]
+            return
+        elif r==6:
+            r=0
+            A=[[1] * 8 for j in range(8)]
+        r=r+1
+        print r
+        dp(A,r,k)
 print dp(l,0,0)
 
