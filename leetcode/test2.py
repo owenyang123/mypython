2befe1f1f0386eb6c7e1b837b7509dc058654395
owenyang123 <owenyang15@gmail.com>
2019-03-29 00:59:43 -0400

    dfs
    dfs

---
+++ leetcode/test2.py
@@ -0,0 +1,31 @@
+str1="abcd"
+str2="efgh"
+l = []
+def pstr(a,b,c):
+    if a=="":
+        l.append(c+b)
+        return
+    if b=="":
+        l.append(c+a)
+        return
+    pstr(a[1:],b,c+a[0])
+    pstr(a,b[1:],c+b[0])
+
+pstr(str1,str2,"")
+print l
+dict1={2:["a","b","c"],3:["d","e","f"],4:["g","h","i"],5:["j","k","l"],6:["m","n","0"],7:["p","q","r","s"],8:["t","u","v"],9:["w","x","y","z"]}
+str2="72245"
+s=[]
+def  traver(a,c):
+    if len(a)==0 or not a:
+        return
+    if len(a)==1:
+        tmp=c
+        for i in dict1[int(a[0])]:
+            s.append(tmp+i)
+        return
+    for i in dict1[int(a[0])]:
+        traver(a[1:],c+i)
+traver(str2,"")
+print s
+
