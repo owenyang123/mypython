74aa246aba74ee9d47b167bb7384835fec170660
owenyang123 <owenyang15@gmail.com>
2019-04-01 21:39:11 -0400

    1
    1

--- amazon/8cell.py
+++ amazon/8cell.py
@@ -1,25 +1,17 @@
-l = []
+
 def cells(list1,n):
-    if n==0:
-        l.append(list1)
-        return l[-1]
-    else:
-        k=[]
-        m=[]
-        for i in range(1,len(cells(list1,n-1))-1):
-            if cells(list1,n-1)[i-1]+cells(list1,n-1)[i+1]==1:
-                k.append(i)
-        for i in range(len(cells(list1,n-1))):
-            if i in k:
-                m.append(1)
-            else:
-                m.append(0)
-        l.pop(0)
+    l = []
+    l.append(list1)
+    for i in range(1,n+1):
+        k=l[i-1]
+        m=len(list1)*[0]
+        print m
+        for j in range(1,len(m)-1):
+            if k[j-1]+k[j+1]==1:
+                m[j]=1
         l.append(m)
-        return l[-1]
+    return l
 
-
-list1=[0, 0, 0, 1, 1, 0, 1, 0]
-print  cells(list1,1)
-print  cells(list1,2)
-print  cells(list1,3)
+print cells([1,1,1,0,1,1,1,1],1)
+print cells([1,1,1,0,1,1,1,1],2)
+print cells([1,1,1,0,1,1,1,1],3)
