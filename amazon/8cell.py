e8b38a412ecb2268da0e3ffdd8c3782ea992f962
owenyang123 <owenyang15@gmail.com>
2019-04-01 21:38:41 -0400

    1
    1

--- amazon/8cell.py
+++ amazon/8cell.py
@@ -1,25 +1,28 @@
-l = []
+
 def cells(list1,n):
-    if n==0:
-        l.append(list1)
-        return l[-1]
-    else:
-        k=[]
-        m=[]
-        for i in range(1,len(cells(list1,n-1))-1):
-            if cells(list1,n-1)[i-1]+cells(list1,n-1)[i+1]==1:
-                k.append(i)
-        for i in range(len(cells(list1,n-1))):
-            if i in k:
-                m.append(1)
-            else:
-                m.append(0)
-        l.pop(0)
+    list2=[0]
+    list2=list2+list1+[0]
+    l = []
+    l.append(list2)
+    for i in range(1,n+1):
+        k=l[i-1]
+        m=len(list2)*[0]
+        for j in range(1,len(m)-1):
+            if k[j-1]+k[j+1]==1:
+                m[j]=1
         l.append(m)
-        return l[-1]
+    return l[-1][1:-1]
 
 
-list1=[0, 0, 0, 1, 1, 0, 1, 0]
-print  cells(list1,1)
-print  cells(list1,2)
-print  cells(list1,3)
+list1=[1,1,1,1,1,1,1,1]
+t=0
+l=list1=[1,1,1,1,1,1,1,1]
+c=0
+while (t==0):
+    l=cells(l,1)
+    if l==list1:
+        t=1
+        print c
+
+    else:
+        c=c+1
---
+++ mandd.py
@@ -0,0 +1,15 @@
+import random
+for i in range(1,10):
+    k1=random.randint(1, 10)
+    k2=random.randint(1, 10)
+    k3=random.randint(1, 10)
+    k4=random.randint(1, 10)
+    print str(k1)+"/"+str(k2)+"+"+str(k3)+"/"+str(k4)+"="
+
+for i in range(1,10):
+    k1=random.randint(100, 1000)
+    k2=random.randint(1, 99)
+    k3=random.randint(100, 1000)
+    k4=random.randint(1, 10)
+    print str(k1)+"*"+str(k2)+"="
+    print str(k3) + "/" + str(k4) + "="
