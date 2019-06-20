e6dfcd26351b2d00b4009387bbc2338de270338a
owenyang123 <owenyang15@gmail.com>
2019-06-20 00:35:43 -0400

    threading

---
+++ junosautomation/Dev_Ops_class.py
@@ -0,0 +1,7 @@
+import jnpr
+class JNPRdevops:
+    def healthcheck(self, A):
+        if not A:
+            print "not valid"
+            return None
+        return None
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,4 +1,15 @@
-keys = ['1001', '1002', '1003']
-values = ['1', '2', '23']
-d = dict(zip(keys, values))
-print d
+import threading
+import time
+
+def show(arg,a):
+    time.sleep(1)
+    print('thread '+str(arg)+" running....")
+    print a
+
+if __name__ == '__main__':
+    print time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
+    for i in range(100):
+        t = threading.Thread(target=show, args=(i,str(i)))
+        t.start()
+    t.join()
+    print time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
