4ac98a31a0860ee2146c625f5fe1814fc2e840e6
owenyang123 <owenyang15@gmail.com>
2019-08-23 00:37:55 -0400

    bayesdone
    bayesdone

--- amazon/Thomas Bayes.py
+++ amazon/Thomas Bayes.py
@@ -1,5 +1,4 @@
 # some balls in box A,B.Box A has 70 reds,30 greens,Box B has 30 reds,70 greens
-
 default_red=0.5
 default_green=0.5
 default_A=0.5
@@ -9,15 +8,24 @@ default_GA=1-default_RA
 default_RB=0.3
 default_GB=1-default_RB
 
-list1=[1,1,1,1,1,1,1,0,0,0,0,0]
+list1=[0,0,0,0,1,1,1,1,1,1,1,1]
 list1.reverse()
-print list1
 
-def bayesfun(pIsBox1, pBox1, pBox2):
-    return (pIsBox1 * pBox1)/((pIsBox1 * pBox1) + (1 - pIsBox1) * pBox2)
+#a:default_RA,b:default_RB,c:default_A,d:default_B
+def bayesfun(a,b,c,d):
+    return (a * c)/((a * c) + (b* d))
 
 while (len(list1)>0):
     if list1[-1]==1:
+        default_A=bayesfun(default_RA,default_RB,default_A,default_B)
+        default_B=1-default_A
+        print default_A
+        list1.pop()
+    else:
+        default_A=bayesfun(default_GA,default_GB,default_A,default_B)
+        default_B=1-default_A
+        print default_A
+        list1.pop()
         
 
 
