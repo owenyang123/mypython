94a361ce1b0c213886118ccc0f3938d41ebec82f
owenyang123 <owenyang15@gmail.com>
2019-09-16 10:33:42 -0400

    dfs
    dfs

--- amazon/ipaddress.py
+++ amazon/ipaddress.py
@@ -19,3 +19,34 @@ class Solution:
 
 k=Solution()
 print k.allipaddress("1045894")
+
+
+
+
+given_number = "1045894"
+
+def parse_all_ip_address(n):
+
+    return get_next_sec([], str(n))
+
+    # end parse_all_ip_address
+
+class solutions1:
+    def ipadd(self,ipstr):
+        if len(ipstr)>12 or len(ipstr)<4:
+            return None
+        coms=[]
+        self.dfs(ipstr,"",coms)
+        return coms
+    def dfs(self,ipstr,tempstr,coms):
+        if tempstr.count(".")==3:
+            if ipstr!="" and int(ipstr)<256:
+                coms.append(tempstr+ipstr)
+            return
+        else:
+            for i in range(1,4):
+                if i<len(ipstr ) and int(ipstr[0:i])<256 and ipstr[i]!="0":
+                    self.dfs(ipstr[i:],tempstr+ipstr[0:i]+".",coms)
+
+k=solutions1()
+print k.ipadd("1045894")
