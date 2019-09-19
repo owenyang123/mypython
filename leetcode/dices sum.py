454fa2f91bc3fba90a3528730ae3e35a61b8ac95
owenyang123 <owenyang15@gmail.com>
2019-09-18 22:42:56 -0400

    1
    1

--- leetcode/dices sum.py
+++ leetcode/dices sum.py
@@ -1,26 +1,26 @@
 class Solution:
     def dicesSum(self, n):
-        po=1
+        po=1.0
         for i in range(n):
-            po=po*0.17
+            po=(po*1.0)/6.0
         l=[1,2,3,4,5,6]
         l1=[]
         l2=[0]*(n-1)
         self.dfs(l,l1,l2,n,n)
         dict1={}
         for i in l1:
-            if sum(i) not in dict1.keys():
-                dict1[sum(i)]=po
+            if i not in dict1.keys():
+                dict1[i]=po
             else:
-                dict1[sum(i)]+=po
+                dict1[i]+=po
         result=[]
         for i in dict1.keys():
-            result.append([i,round(dict1[i],2)])
+            result.append([i,dict1[i]])
         return result
     def dfs(self,l,l1,l2,n,n1):
         if n==1:
             for i in l:
-                l1.append((l2+[i]))
+                l1.append(sum((l2+[i])))
             return l1
         else:
             for i in l :
@@ -29,4 +29,4 @@ class Solution:
 
 
 k=Solution()
-print k.dicesSum(4)
+print k.dicesSum(7)
