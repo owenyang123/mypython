36cf49243095c2e0d165e1562399fdaffb2d8d66
owenyang123 <owenyang15@gmail.com>
2019-09-23 16:12:15 -0400

    flight
    1

---
+++ leetcode/flight.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def findCheapestPrice(self, n, flights, src, dst, K):
+        # write your code here
+        distance = [sys.maxsize for i in range(n)]
+        distance[src] = 0
+
+        for i in range(0, K + 1):
+            dN = list(distance)
+            for u, v, c in flights:
+                dN[v] = min(dN[v], distance[u] + c)
+            distance = dN
+            print distance
+
+        if distance[dst] != sys.maxsize:
+            return distance[dst]
+        else:
+            return -1
