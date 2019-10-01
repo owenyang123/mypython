c1c5e730de0ddbb9aa6dec54ca9fd74a489fab59
owenyang123 <owenyang15@gmail.com>
2019-10-01 16:11:12 -0400

    1
    1

--- leetcode/flight.py
+++ leetcode/flight.py
@@ -1,9 +1,10 @@
+import sys
 class Solution:
-
     def findCheapestPrice(self, n, flights, src, dst, K):
         # write your code here
         distance = [sys.maxsize for i in range(n)]
         distance[src] = 0
+        print distance
 
         for i in range(0, K + 1):
             dN = list(distance)
@@ -16,3 +17,7 @@ class Solution:
             return distance[dst]
         else:
             return -1
+
+k=Solution()
+
+print k.findCheapestPrice(4,[[0,1,100],[1,2,100],[0,2,500],[2,3,500]],0,2,1)
