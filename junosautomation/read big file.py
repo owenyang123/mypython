96147866977a1edbece1392fa5164b1f903f191d
owenyang123 <owenyang15@gmail.com>
2019-10-04 09:25:01 -0400

    1
    1

--- junosautomation/read big file.py
+++ junosautomation/read big file.py
@@ -1,11 +1,12 @@
 import os
 import re
-
+sum=0
 with open("bgp summary", 'r') as f:
     for i in f:
         if re.search("Est",i):
-            print i.split()
-
+            print [i.split()[0],i.split()[-1]]
+            sum=sum+1
+print sum
 
 str = "The SraiSn in Spain123 456"
 x = re.search(r"\bS\w+", str)
--- leetcode/Binary Tree Level Order Traversal.py
+++ leetcode/Binary Tree Level Order Traversal.py
@@ -1,7 +1,10 @@
 class Solution:
     """
-    @param root: A Tree
-    @return: Level order a list of lists of integer
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
     """
     def levelOrder(self, root):
         # write your code here
