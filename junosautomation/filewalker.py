6f3416705562f0195a1385537a477d882521da84
owenyang123 <owenyang15@gmail.com>
2019-10-09 23:15:08 -0400

    1
    1

---
+++ junosautomation/filewalker.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+path123 = raw_input('please input path: ')
+reg = r'(\.py$)'
+pdfre = re.compile(reg)
+count=0
+for path, d, filelist in os.walk(path123):
+    for filename in filelist:
+        t = os.path.join(path, filename)
+        if re.findall(pdfre, t):
+            count+=1
+            print count
+
+
+def walker(filepath,filetype):
+    reg = filetype
+    pdfre = re.compile(reg)
+    for path, d, filelist in os.walk(filepath):
+        for filename in filelist:
+            t = os.path.join(path, filename)
+            if re.findall(pdfre, t):
+                print t
+
+
+walker("D:\Python272018\owenpython2018\mypython","py")
