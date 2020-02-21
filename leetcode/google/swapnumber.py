6744a3b7b16c249b9bd06c00ac9629a95395b1fc
owenyang123 <owenyang15@gmail.com>
2020-02-21 00:54:22 -0500

    1
    1

--- leetcode/google/swapnumber.py
+++ leetcode/google/swapnumber.py
@@ -4,12 +4,12 @@ def swapnumber(num):
     if len(str1)==1:
         return num
     list1=[x for x in str1]
-    if list1[0]==min(list1):
+    min1 = min(list1)
+    if list1[0]==min1:
         return int(str(list1[0])+str(swapnumber(int("".join(list1[1:])))))
-    min1=min(list1)
     for i in range(len(list1)-1,-1,-1):
         if list1[i]==min1:
             list1[0],list1[i]=list1[i],list1[0]
     return int("".join(list1))
 
-print swapnumber(1992)
+print swapnumber(11111992)
