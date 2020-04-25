a9a59c4b7b8cdfedc6c3569986ec93661b32175b
owenyang123 <owenyang15@gmail.com>
2020-04-25 00:30:59 -0400

    1
    1

--- optionstrading/123.py
+++ optionstrading/123.py
@@ -1,5 +1,5 @@
-import basictools as bt
-
-result=bt.get_stock_data("2020-01-13","2020-04-13","OXY","AAPL")
-
-print result.keys()
+import yfinance as yf
+aapl = yf.Ticker("AAPL")
+data=aapl.option_chain('2020-04-30').calls
+print aapl.info
+print data['ask']
