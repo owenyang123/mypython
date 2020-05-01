36dc157e1db4dc69718ac2a451ba11874c062a0e
owenyang123 <owenyang15@gmail.com>
2020-04-30 23:06:32 -0400

    1
    1

--- optionstrading/monitorcall.py
+++ optionstrading/monitorcall.py
@@ -21,8 +21,7 @@ if __name__ == "__main__":
         for i in earningdate:
             if bt.get_date_delta(earningdate[i],cur_date)<=20:
                 msg+=i +" "+str(bt.get_date_delta(earningdate[i],cur_date))+" ,"
-        print msg
-        #if msg:bt.mail_notice(msg,*maillist)
+        if msg:bt.mail_notice(msg,*maillist)
         time.sleep(172800)
 
 
