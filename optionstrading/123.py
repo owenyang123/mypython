2beac70bf90ca507c10217f07427e7009125ab22
owenyang123 <owenyang15@gmail.com>
2020-04-30 22:22:47 -0400

    1
    1

--- optionstrading/123.py
+++ optionstrading/123.py
@@ -3,14 +3,6 @@ import numpy as np
 import time
 import datetime
 import basictools as bt
-import pandas as pd
 import os
 
-
-while (1):
-    aapl = yf.Ticker("MO")
-    print (aapl.major_holders)
-    print (aapl.calendar)
-    print (sorted(aapl.options))
-    print aapl.option_chain('2020-05-07').calls.loc[:,["ask","bid","strike"]]
-    time.sleep(180)
+print bt.get_options_data("2020-05-07","call",*['MO'])['MO'].loc[lambda x:x['strike']==39.5].loc[:,["ask","bid","strike"]]
