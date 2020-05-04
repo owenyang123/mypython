2507d63c3935dcb1d016f1757b95904c8372d68f
owenyang123 <owenyang15@gmail.com>
2020-05-03 22:46:38 -0400

    1
    1

--- optionstrading/dumpstock100days.py
+++ optionstrading/dumpstock100days.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
     content = pd.read_html(url)
     stocklist = content[0]['Symbol'].tolist()
-    with open('1.html', 'w') as file:
+    with open('/var/www/html/1.html', 'w') as file:
         file.write('<html> \n')
         file.write('    <head> \n')
         file.write('        <title>Stock Data</title> \n')
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     for i in stocklist:
         try:
             data1[i].plot(subplots=False, figsize=(10, 4))
-            str1=i+".png"
+            str1="/var/www/html/"+i+".png"
             plt.savefig(str1)
             plt.clf()
             plt.cla()
