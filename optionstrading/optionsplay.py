029679aca1c00738545910a49d0b18b58b09f3c1
owenyang123 <owenyang15@gmail.com>
2020-05-06 22:03:52 -0400

    1
    1

--- optionstrading/optionsplay.py
+++ optionstrading/optionsplay.py
@@ -10,7 +10,7 @@ import os
 import time
 sns.set(style="whitegrid")
 
-str1="LNT ,AMCR ,D ,ANSS ,ALB ,UAA ,LNC ,PWR ,CDW ,SBAC ,HII ,AES ,CTSH ,FRT ,ATO ,RCL ,EXR ,MET ,ES ,EOG ,AWK ,IRM ,SYY ,FISV ,FLT ,LIN ,CF ,MRO ,HPQ ,ESS ,DISH ,FTNT ,EQIX ,CVS ,KIM ,ZTS ,TMUS ,AEP ,J ,FOXA ,ED ,FIS ,BWA ,ZBH ,MAA ,BR ,COTY ,MYL ,AMAT ,AEE ,UDR ,DLR ,MAR ,FLIR ,QRVO ,AMP ,CTVA ,MCHP ,VFC ,DISCK ,NRG ,MTD ,CNP ,IFF ,NWSA ,NLOK ,CSCO ,GPN ,BLL ,ALXN ,REG ,WYNN ,VMC ,EVRG ,NCLH ,GPC ,BKNG ,EXC ,BDX ,APA ,PYPL "
+str1="LNT ,AMCR ,D ,ANSS ,ALB ,UAA ,LNC ,PWR ,CDW ,SBAC ,HII ,AES ,CTSH ,FRT ,ATO ,RCL ,EXR ,MET ,ES ,EOG ,AWK ,IRM ,SYY ,FISV ,FLT ,LIN ,CF ,MRO ,HPQ ,ESS ,DISH ,FTNT ,EQIX ,CVS ,KIM ,ZTS ,TMUS ,AEP ,J ,FOXA ,ED ,FIS ,BWA ,ZBH ,MAA ,BR ,COTY ,MYL ,AMAT ,AEE ,UDR ,DLR ,MAR ,FLIR ,QRVO ,AMP ,CTVA ,MCHP ,VFC ,DISCK ,NRG ,MTD ,CNP ,IFF ,NWSA ,NLOK ,CSCO ,GPN ,BLL ,ALXN ,REG ,WYNN ,VMC ,EVRG ,NCLH ,GPC ,BKNG ,EXC ,BDX ,APA ,PYPL"
 stocklist=str1.replace(" ","").split(",")
 earningdate = bt.get_next_event(*stocklist)
 latest_option_date={}
@@ -51,9 +51,12 @@ for i in latest_option_date:
         pricelist=bt.perdict10days(startprice, mu, dt, sigma, days=10)
         if bt.incornot(list(pricelist))>0.02:temp1+=1
     temp1=float(temp1)/100
-    if bt.incornot(days0to30_data[i]['Adj Close'].tolist())>0:temp2=1
-    if bt.incornot(days30to60_data[i]['Adj Close'].tolist()) > 0: temp3 = 1
-    if bt.incornot(days60to90_data[i]['Adj Close'].tolist()) > 0: temp4 = 1
+    if bt.incornot(days0to30_data[i]['Adj Close'].tolist())>0.3:temp2=1.5
+    elif bt.incornot(days0to30_data[i]['Adj Close'].tolist())>0:temp2=1
+    if bt.incornot(days30to60_data[i]['Adj Close'].tolist()) > 0.3: temp3 = 1.5
+    elif bt.incornot(days30to60_data[i]['Adj Close'].tolist()) > 0:temp3=1
+    if bt.incornot(days60to90_data[i]['Adj Close'].tolist()) > 0.3: temp4 = 1.5
+    elif bt.incornot(days60to90_data[i]['Adj Close'].tolist()) > 0:temp4 = 1
     p=sum(probability_rate*np.array([temp1,temp2,temp3,temp4]))/sum(probability_rate)
     corp=""
     if p>=0.6:corp="call"
@@ -74,8 +77,8 @@ for i in latest_option_date:
     if optiondata_put[i].loc[lambda df: df['strike'] == target][["bid", "ask"]].sum(axis=1).tolist() != []:
         option_put_price = optiondata_put[i].loc[lambda df: df['strike'] == target][["bid", "ask"]].sum(axis=1).tolist()[0]/2
     else:option_put_price=100
-    b_call=((startprice*0.04-option_call_price)/option_call_price)
-    b_put=((startprice*0.04-option_put_price)/option_put_price)
+    b_call=((startprice*0.03-option_call_price)/option_call_price)
+    b_put=((startprice*0.03-option_put_price)/option_put_price)
     if kelly_data[i][0]=="call":kelly_data[i].append(b_call)
     if kelly_data[i][0] == "put": kelly_data[i].append(b_put)
 
@@ -83,4 +86,6 @@ for i in kelly_data:
     if kelly_data[i][0]!="hold":kelly_data[i].append(bt.kelly_caculation(kelly_data[i][3],kelly_data[i][4]))
     else:kelly_data[i]=[]
 
-print kelly_data
+for i in kelly_data:
+    if kelly_data[i]==[]:continue
+    print(kelly_data[i])
