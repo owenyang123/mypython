0c3ea361d6e5eb08dc725f2b6c83c1eb1f0e6c47
owenyang123 <owenyang15@gmail.com>
2020-05-11 10:38:00 -0400

    1
    1

--- optionstrading/ymh.py
+++ optionstrading/ymh.py
@@ -11,6 +11,28 @@ import optionsplay as op
 import stockplay as sp
 sns.set(style="whitegrid")
 import stockplay as sp
-str1='AMCR ,UAA ,RCL ,LIN ,KIM ,ZBH ,BR ,COTY ,MYL ,AMAT ,AEE ,MAR ,VFC ,IFF ,NLOK ,CSCO ,NCLH ,EXC'
+'''
+str1='COST,TQQQ'
 stocklist = str1.replace(" ","").split(",")
 print op.caifuziyou(stocklist)
+
+print sp.caifuziyou(stocklist)
+'''
+
+def rmvdate(str1):
+    if not str1:return []
+    list1=str1.replace(" ","").split(",")
+    temp=[]
+    for i in list1:
+        for j in range(len(i)):
+            if i[j]=="2":
+                temp.append((i[0:j],i[j:]))
+                break
+    return temp
+
+
+str1='CSCO2020-05-13 ,NCLH2020-05-14 ,TTWO2020-05-20 ,VFC2020-05-15 ,ZBH2020-05-11 ,JWN2020-05-19 ,AEE2020-05-11 ,AAP2020-05-19 ,A2020-05-21 ,MAR2020-05-11 ,WMT2020-05-19 ,HRL2020-05-21 ,COTY2020-05-11 ,IFF2020-05-11 ,CPRT2020-05-20 ,LOW2020-05-20 ,ROST2020-05-21 ,SNPS2020-05-20 ,PGR2020-05-20 ,ADI2020-05-20 ,HPE2020-05-21 ,LB2020-05-20 ,UAA2020-05-11 ,AMCR2020-05-11 ,NLOK2020-05-14 ,KSS2020-05-19 ,BBY2020-05-21 ,NVDA2020-05-21 ,TJX2020-05-21 ,HD2020-05-19 ,MDT2020-05-21 ,MYL2020-05-11 ,INTU2020-05-21 ,AMAT2020-05-14 ,DXC2020-05-21 '
+
+stocklist= [i[0] for i in rmvdate(str1)]
+print op.caifuziyou(stocklist)
+print sp.caifuziyou(stocklist)
