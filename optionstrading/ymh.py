644b809abad1ffc5c4c398ba3abf555fb29a4b76
owenyang123 <owenyang15@gmail.com>
2020-05-19 16:04:33 -0400

    1
    1

--- optionstrading/ymh.py
+++ optionstrading/ymh.py
@@ -8,18 +8,10 @@ import pandas as pd
 import csv
 
 
-url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-content = pd.read_html(url)
-stocklist = content[0]['Symbol'].tolist()
+
 
 str1='AAP ,LB ,HPQ ,KEYS ,INTU ,RCL ,NTAP ,ADI ,HPE ,DG ,DXC ,ROST ,JWN ,DLTR ,AZO ,NVDA ,TJX ,DE ,PVH ,A ,CPRT ,KSS ,PGR ,MDT ,HRL ,BBY ,HD ,TTWO ,COST ,LOW ,ULTA ,ADSK ,SNPS '
 list1=str1.replace(",","").split(" ")
-l=sp.caifuziyou(list1)
-with open(r'ymh.csv','a') as fd:
-    for t in l:
-        if t[-1]!=0:
-            writer=csv.writer(fd)
-            writer.writerow([bt.get_data(0)]+t)
 l=op.caifuziyou(list1)
 with open(r'ymh.csv','a') as fd:
     for t in l:
@@ -27,6 +19,9 @@ with open(r'ymh.csv','a') as fd:
             writer=csv.writer(fd)
             writer.writerow([bt.get_data(0)]+t)
 '''
+url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+content = pd.read_html(url)
+stocklist = content[0]['Symbol'].tolist()
 =======
 str1="CPB 17 ,JWN 11 ,NTAP 10 ,SNPS 3 ,CPRT 3 ,TJX 4 ,COST 11 ,MDT 4 ,LB 3 ,PGR 3 ,A 4 ,KEYS 9 ,HRL 4 ,ROST 4 ,KSS 2 ,NVDA 4 ,ADSK 10 ,HPE 4 ,PVH 10 ,BBY 4 ,TTWO 3 ,DG 11 ,DE 5 ,AAP 2 ,INTU 4 ,GPS 11 ,ULTA 11 ,DLTR 11 ,LOW 3 ,HPQ 10 ,TIF 16 ,WMT 2 ,HD 2 ,ADI 3 ,AZO 9 ,DXC 11 ,"
 l1=[]
