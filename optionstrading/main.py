e7bd925235df3daa337e65e439be1a1fb7861d29
owenyang123 <owenyang15@gmail.com>
2020-05-21 01:50:55 -0400

    1
    1

--- optionstrading/main.py
+++ optionstrading/main.py
@@ -1,7 +1,7 @@
-import csv
-l=[]
-with open('ymh.csv') as fd:
-    for i in fd.readlines():
-        l.append(i.replace("\n","").split(","))
-l.sort(key=lambda x:x[-3])
-print l
+l=[1]
+print l[:-1]
+
+import stockplay as sp
+import optionsplay as op
+
+print sp.caifuziyou(['nwsa'])
