e360db794e52799b49e8264705a5fd3d984b0b88
owenyang123 <owenyang15@gmail.com>
2020-05-21 01:50:19 -0400

    1
    1

--- optionstrading/main.py
+++ optionstrading/main.py
@@ -9,25 +9,4 @@ import csv
 import yfinance as yf
 sns.set(style="whitegrid")
 if __name__ == "__main__":
-    alllist=[]
-    with open('nyselist') as f:
-        for i in f.readlines():
-            if "-" not in i and "." not in i:
-                alllist.append(i.replace("\n", ""))
-    with open('nsdqlist') as f:
-        for i in f.readlines():
-            if "-" not in i and "." not in i:
-                alllist.append(i.replace("\n", ""))
-    alllist.sort()
-    for i in alllist:
-        if i.isalpha():
-            try:
-                temp = yf.Ticker(i)
-                if temp.info and temp.info['enterpriseValue']>10000000000:
-                    l=sp.caifuziyou([i])
-                    with open(r'ymh.csv', 'a') as fd:
-                        for t in l:
-                            if t[-1] != 0 and t[-3]>0.9:
-                                writer = csv.writer(fd)
-                                writer.writerow([bt.get_data(0)] + t+[temp.info['enterpriseValue']])
-            except:pass
+    print sp.caifuziyou(['XLE'])
--- optionstrading/test.py
+++ optionstrading/test.py
@@ -11,15 +11,15 @@ with open('nsdqlist') as f:
     for i in f.readlines():
         if "-" not in i and "." not in i:
             alllist.append(i.replace("\n", ""))
-set1=set([])
+set1=set(['Financial','Energy','Financial Services','Consumer Cyclical','Basic Materials','Communication Services','Industrials','Healthcare','Real Estate','Utilities','Technology','Consumer Defensive'])
 for i in alllist:
     try:
         temp=yf.Ticker(i)
-        set1.add(temp.info['sector'])
+        if temp.info['sector'] in set1 and temp.info['enterpriseValue']>1000000000:
+            str1=temp.info['sector']+".csv"
+            with open(str1, 'a') as fd:
+                writer = csv.writer(fd)
+                writer.writerow([i])
     except:pass
 
-for i in set1:
-    str1=i+".csv"
-    with open(str1, 'a') as fd:
-        writer = csv.writer(fd)
-        writer.writerow([bt.get_data(0)])
+
