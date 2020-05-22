ce94cb5462256343ab8a4c07ffdfed17816047b6
owenyang123 <owenyang15@gmail.com>
2020-05-22 11:49:15 -0400

    1

--- optionstrading/monitorcall.py
+++ optionstrading/monitorcall.py
@@ -15,21 +15,16 @@ if __name__ == "__main__":
             if not stocklist: exit()
             print("starting polling date from yahoo")
             earningdate = bt.get_next_event(*stocklist)
-            print earningdate
-            if not earningdate:print "1231"
+            print(earningdate)
             cur_date=str(datetime.date.today())
             msg=""
             print("finding the delta less then 10days")
             for i in earningdate:
                 if 0<bt.get_date_delta(earningdate[i],cur_date)<=10:
-                    msg+=i +" "+" ,"
-            #if msg:bt.mail_notice(msg[:-1],*maillist)
-            print msg[:-1]
-            time.sleep(40000)
+                    msg+=i +" "+str(bt.get_date_delta(earningdate[i],cur_date))+" ,"
+            if msg:bt.mail_notice(msg,*maillist)
+            print(msg+"123")
+            time.sleep(10000)
         except:
             print ("error seen")
             pass
-
-
-
-
