93c33ed114cb84b628a82703b967b6ee38e263cb
owenyang123 <owenyang15@gmail.com>
2020-05-24 01:56:22 -0400

    1
    1

--- optionstrading/dailysp500.py
+++ optionstrading/dailysp500.py
@@ -12,11 +12,14 @@ sns.set(style="whitegrid")
 url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 content = pd.read_html(url)
 stocklist = content[0]['Symbol'].tolist()
-
+set1=set()
+with open('downsharp') as fd:
+    for i in fd.readlines():
+        set1.add(i.replace("\n",""))
 l=sp.caifuziyou(stocklist)
 with open(r'today.csv', 'w') as fd:
     for t in l:
-        if t[-1] != 0:
+        if t[-1] != 0 and t[0] in set1:
             writer = csv.writer(fd)
             writer.writerow([bt.get_data(0)]+t)
 
