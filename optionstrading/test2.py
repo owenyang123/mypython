2d711bf91fab3303cfc6ba522fff96abd8f249e7
owenyang123 <owenyang15@gmail.com>
2020-05-31 01:18:10 -0400

    1
    1

--- optionstrading/test2.py
+++ optionstrading/test2.py
@@ -7,9 +7,39 @@ import optionsplay as op
 import stockplay as sp
 import yfinance as yf
 import yahoofinancials as yf1
-with open('2020-05-28.csv') as fd:
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import basictools as bt
+import time
+import stockplay as sp
+import csv
+import yfinance as yf
+import optionsplay as op
+
+filename_tod=bt.get_data(0)+".csv"
+filename_yes=bt.get_data(1)+".csv"
+set_tod=set([])
+set_yes=set([])
+with open(filename_tod) as fd:
     for i in fd.readlines():
-        if len(i)>10:print i.replace("\n","").split(",")[1]
+        if len(i) > 10:
+            set_tod.add(i.replace("\n", "").split(",")[0])
+with open(filename_yes) as fd:
+    for i in fd.readlines():
+        if len(i) > 10:
+            set_yes.add(i.replace("\n", "").split(",")[1])
+print set_tod
+print set_yes
+for i in set_tod:
+    if i not in set_yes:
+        print i
+
+
+
+
+
 
 '''
 nyse=[]
