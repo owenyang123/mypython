cdcd291ee344b6e102967ce1e0fe59ad79c203af
owenyang123 <owenyang15@gmail.com>
2020-06-02 09:53:38 -0400

    1
    1

--- optionstrading/test2.py
+++ optionstrading/test2.py
@@ -6,7 +6,6 @@ import csv
 import optionsplay as op
 import stockplay as sp
 import yfinance as yf
-import yahoofinancials as yf1
 import numpy as np
 import seaborn as sns
 import pandas as pd
@@ -18,24 +17,7 @@ import csv
 import yfinance as yf
 import optionsplay as op
 
-filename_tod=bt.get_data(0)+".csv"
-filename_yes=bt.get_data(1)+".csv"
-set_tod=set([])
-set_yes=set([])
-with open(filename_tod) as fd:
-    for i in fd.readlines():
-        if len(i) > 10:
-            set_tod.add(i.replace("\n", "").split(",")[0])
-with open(filename_yes) as fd:
-    for i in fd.readlines():
-        if len(i) > 10:
-            set_yes.add(i.replace("\n", "").split(",")[1])
-print set_tod
-print set_yes
-for i in set_tod:
-    if i not in set_yes:
-        print i
-
+print sp.caifuziyou(['MELI'])
 
 
 
