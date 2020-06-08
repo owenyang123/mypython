e12b83cf63e2d3dbf1d004316203cc05e72391fe
owenyang123 <owenyang15@gmail.com>
2020-06-08 00:00:02 -0400

    1
    1

---
+++ leetcode/bfs.py
@@ -0,0 +1,17 @@
+dict1={1:[2,3,4],2:[3,4],3:[5],4:[5],5:[]}
+def dfstm(dict1,start,end):
+    if start not in dict1 or end not in dict1:return []
+    pathlist=[]
+    q=[[start,[]]]
+    while (q):
+        print q
+        node,path=q.pop(0)
+        if node==end:
+            pathlist.append(path+[node])
+            continue
+        for x in dict1[node]:
+            q.append([x,path+[node]])
+    return pathlist
+x=dfstm(dict1,1,5)
+print "==========="
+print x
