17edc4396d2c0dc960f69365fc4144120fbd9c05
owenyang123 <owenyang15@gmail.com>
2021-03-04 01:05:14 -0500

    1
    1

--- leetcode/google/preparegoogle.py
+++ leetcode/google/preparegoogle.py
@@ -82,17 +82,15 @@ class Solution(object):
                 return temp
         return 0
 
+
 class Solution(object):
     def getImportance(self, employees, id):
-        """
-        :type employees: Employee
-        :type id: int
-        :rtype: int
-        """
-        # Time: O(n)
-        # Space: O(n)
-        emps = {employee.id: employee for employee in employees}
-        def dfs(id):
-            subordinates_importance = sum([dfs(sub_id) for sub_id in emps[id].subordinates])
-            return subordinates_importance + emps[id].importance
+        A = {}
+
+        def dfs(i):
+            return A[i][0] + sum(dfs(j) for j in A[i][1])
+
+        for e in employees:
+            A[e.id] = [e.importance, e.subordinates]
+
         return dfs(id)
