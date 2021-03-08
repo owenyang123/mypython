4cdab60a0790f348ce2057cd4568920b7a77d9cf
owenyang123 <owenyang15@gmail.com>
2021-03-08 00:59:33 -0500

    1
    1

--- leetcode/google/preparegoogle.py
+++ leetcode/google/preparegoogle.py
@@ -143,3 +143,16 @@ class Solution(object):
             temp2=[0]+[j for j in res[i-1] ]
             res.append([temp1[j]+temp2[j] for j in range(len(temp1))])
         return res
+
+class Solution:
+    def floodFill(self, image, sr, sc, newColor):
+        old, m, n = image[sr][sc], len(image), len(image[0])
+        if old != newColor:
+            q = collections.deque([(sr, sc)])
+            while q:
+                i, j = q.popleft()
+                image[i][j] = newColor
+                for x, y in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
+                    if 0 <= x < m and 0 <= y < n and image[x][y] == old:
+                        q.append((x, y))
+        return image
