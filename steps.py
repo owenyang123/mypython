d282d48363a2a729faeb9cef5ee9e7722636dd25
owenyang123 <owenyang15@gmail.com>
2021-04-07 22:15:06 -0400

    1
    1

--- steps.py
+++ steps.py
@@ -6,7 +6,7 @@ step_num=int(input("input max steps:"))
 l=[]
 for x in range(0, step_num+1):
     l.append(x)
-print l
+print(l)
 l[0]=1
 l[1]=1
 l[2]=2
@@ -14,5 +14,5 @@ l[3]=4
 for x in range(4,step_num+1):
     t=l[x-1]+l[x-2]+l[x-3]
     l[x]=t
-    print x,l
+    print(x,l)
 
